d648af757fbd140e994069dec20749502cf3580f
Tiger Cross <tc2017@ic.ac.uk>
2019-12-24 16:08:04 +0000

    Begin implementing new idea form.

--- good_ideas_machine/lib/newidea.dart
+++ good_ideas_machine/lib/newidea.dart
@@ -8,15 +8,32 @@ class NewIdeaPage extends StatefulWidget {
 
 class _NewIdeaPageState extends State<NewIdeaPage> {
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Coming soon...")
-          ],
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            vertical: 16.0,
+            horizontal: 16.0,
+          ),
+          child:
+          Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text("Record New Idea", textScaleFactor: 2),
+                TextFormField(),
+                TextFormField(),
+                Slider(value: 0, onChanged: null),
+                Slider(value: 0, onChanged: null),
+                Slider(value: 0, onChanged: null),
+              ],
+            ),
+          )
         ),
       ),
       floatingActionButton: FloatingActionButton(
