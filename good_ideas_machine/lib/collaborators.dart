50e2461b0b6cdd63f3e31c1194f6aa18edaa05bb
Tiger Cross <tc2017@ic.ac.uk>
2019-12-24 02:04:34 +0000

    Implement navigation using bottomAppBar.

---
+++ good_ideas_machine/lib/collaborators.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CollaboratorsPage extends StatefulWidget {
+
+  @override
+  _CollaboratorsPageState createState() => _CollaboratorsPageState();
+}
+
+class _CollaboratorsPageState extends State<CollaboratorsPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Also coming soon...")
+          ],
+        ),
+      ),
+
+    );
+  }
+}
---
+++ good_ideas_machine/lib/ideas.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class IdeasPage extends StatefulWidget {
+
+  @override
+  _IdeasPageState createState() => _IdeasPageState();
+}
+
+class _IdeasPageState extends State<IdeasPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Also Also coming soon...")
+          ],
+        ),
+      ),
+
+    );
+  }
+}
