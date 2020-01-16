b4109c14900e8574c12ebdfb1aa0757ba40ca5f4
Tiger Cross <tc2017@ic.ac.uk>
2020-01-16 17:08:34 +0000

    Create Idea data model.

---
+++ good_ideas_machine/lib/core/models/Idea.dart
@@ -0,0 +1,26 @@
+class Idea {
+  String title;
+  String description;
+  int originalityRating;
+  int feasibilityRating;
+  int impactRating;
+
+  Idea(this.title, this.description, this.originalityRating,
+      this.feasibilityRating, this.impactRating);
+
+  Idea.fromMap(Map snapshot, String title)
+      : title = title ?? '',
+        description = snapshot['description'] ?? '',
+        originalityRating = snapshot['originalityRating'] ?? '',
+        feasibilityRating = snapshot['feasibilityRating'] ?? '',
+        impactRating = snapshot['impactRating'] ?? '';
+
+  toJson() {
+    return {
+      'description' : description,
+      'originalityRating' : originalityRating,
+      'feasibilityRating' : feasibilityRating,
+      'impactRating' : impactRating,
+    };
+  }
+}
