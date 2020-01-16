8a278ebe783a4953430bc66a8ff838b0e4d17d17
Tiger Cross <tc2017@ic.ac.uk>
2020-01-16 17:10:11 +0000

    Update pubspec and tests.

--- good_ideas_machine/test/widget_test.dart
+++ good_ideas_machine/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:good_ideas_machine/main.dart';
+import 'package:good_ideas_machine/ui/main.dart';
 
 void main() {
 //  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
